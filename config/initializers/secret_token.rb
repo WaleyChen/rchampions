53ad15fe1e6ee5f0aa9bb64be3a2e8a04d67a13b
Waley Chen <wchen@redwoodstrategic.com>
2012-09-03 04:24:26 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cofounderhub::Application.config.secret_token = '9401740f19b3afbc219e81e1d25a6223bbceb5b995ef9a6842ee606a24a3a494faccbf63fd99a33bb0db8756e03369655a19d5c065b372e3ea1562adeff9964d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cofounderhub::Application.config.session_store :cookie_store, key: '_cofounderhub_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cofounderhub::Application.config.session_store :active_record_store
