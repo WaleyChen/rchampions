5237069910f6020df82e13586c62fcfd6fe67216
Waley Chen <wchen@redwoodstrategic.com>
2012-10-10 21:36:06 -0400

    View and Seed Updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -8,27 +9,25 @@
 
 company =  Company.create({
   description: '',
-  name: 'Shopify',
-  link: 'http://campusperks.ca/'
+  name: 'Altera',
+  link: 'http://www.altera.com/'
 })
 company.save
 
-company =  Company.create({
-  description: '',
-  name: 'Redwood Strategic Inc.',
-  link: 'http://campusperks.ca/'
+job = Job.create({
+  apply_link: 'http://ch.tbe.taleo.net/CH03/ats/careers/apply.jsp?org=ALTERA&cws=1',
+  description: { :description => 
+                          ["As a Software Engineer Intern, you will develop new algorithms and enhance the software infrastructure of Altera’s Quartus II CAD system. Areas of new algorithm development include physical synthesis, placement and routing, and incremental compilation techniques. These are among the most computationally difficult problems in Computer-Aided Design, and finding algorithms with better result quality and reduced CPU time is a very challenging problem. Altera’s Quartus II CAD system is a very large and full-featured software program with over 17 million lines of code, leading to many interesting software engineering questions.", 
+                          "The successful candidate's minimum qualifications will include the following:",
+                          "bulletpoint",
+                          "Working towards an undergraduate degree in Engineering Science, Electrical and Computer engineering, computer science or equivalent."] 
+                      },
+  country: 'CA',
+  city: 'Toronto',
+  deadline: 'Rolling Basis',
+  province: 'ON',
+  title: 'Software Engineer Intern'
 })
-company.save
 
-10.times do 
-  job = Job.create({
-    country: 'Canada',
-    city: 'Toronto',
-    deadline: 'Rolling Basis',
-    province: 'Ontario',
-    title: 'Senior Ruby on Rails Software Developer'
-  })
-
-  job.company = company
-  job.save
-end
+job.company = company
+job.save
