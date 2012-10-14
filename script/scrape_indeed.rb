c33e950c948c1d0f01e562ddd8ad985647b38035
Waley Chen <wchen@redwoodstrategic.com>
2012-10-13 20:02:53 -0400

    Got the little mofo

--- script/scrape_indeed.rb
+++ script/scrape_indeed.rb
@@ -3,6 +3,7 @@
 # NOKOGIRI
 # http://nokogiri.org/tutorials/searching_a_xml_html_document.html
 # http://cheat.errtheblog.com/s/nokogiri/
+require 'mechanize'
 require 'net/http'
 require 'nokogiri'
 require 'open-uri'
@@ -14,12 +15,12 @@ resp = Net::HTTP.get_response(URI.parse(url)) # get_response takes an URI object
 data = JSON.parse(resp.body)
 
 first = data['results'].first
-puts first
-puts first['jobtitle']
-puts first['company']
-puts first['city']
-puts first['state']
-puts first['country']
+# puts first
+# puts first['jobtitle']
+# puts first['company']
+# puts first['city']
+# puts first['state']
+# puts first['country']
 
 # {"jobtitle"=>"Custom Software Developer Intern", "company"=>"MTS Systems Corporation", "city"=>"Eden Prairie", "state"=>"MN", "country"=>"US", 
 #   "formattedLocation"=>"Eden Prairie, MN", "source"=>"MTS Systems Corporation", "date"=>"Thu, 11 Oct 2012 16:59:54 GMT", 
@@ -37,18 +38,23 @@ if resp.code == "301"
 end
 
 data = resp.body
-puts resp
-puts data
-
-puts 'NOKOGIRI'
 
 doc = Nokogiri::HTML(open(url))
 
+redirect_to_apply_link = ''
 doc.css('.view_job_link').each do |link|
-  puts link
-  puts link.href
-  puts link.attribute("href")
-  # if link.attribute('class') == 'view_job_link'
-  #   puts link
-  # end
+  redirect_to_apply_link = "http://indeed.com#{ link['href'] }"
 end
+
+# puts 'GOT THE LINK'
+agent = Mechanize.new
+apply_pg = agent.get(redirect_to_apply_link)
+puts apply_pg.uri.to_s 
+
+# url = redirect_to_apply_link
+# resp = Net::HTTP.get_response(URI.parse(url)) # get_response takes an URI object
+
+# if resp.code == "301"
+#   resp = Net::HTTP.get_response(URI.parse(resp.header['location']))
+# end
+
